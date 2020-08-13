df892a492b83d3f0de9f18433dda4347a891ef92
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-08-13 12:41:01 -0500

    finished learn content styling, moving to brew start

--- src/components/LearnContent/LearnContent.js
+++ src/components/LearnContent/LearnContent.js
@@ -10,15 +10,21 @@ const LearnContent = () => (
     
     <div className='contentLearnTwo'>
     <div className='title'>Grind</div>
-      <div className='contentLearnTwoText'>Hello!</div>
+      <div className='contentLearnTwoText'>
+        <p>Getting a fresh and consistent grind is a critical step in great tasting coffee. A ceramic burr grinder does this wonderfully</p>
+      </div>
     </div>
     <div className='contentLearnThree'>
       <div className='title'>Water</div>
-      <div className='contentLearnThreeText'>Learn 3 text</div>
+      <div className='contentLearnThreeText'>
+        <p>You'll want a kettle that can hold around 1L of water while also being able to pour in small, controlled amounts. The gooseneck is king for this.</p>
+      </div>
     </div>
     <div className='contentLearnFour'>
     <div className='title'>Scale</div>
-    <div className='contentLearnFourText'>Ability to measure responsively and reliably is key. Wide base and a built in timer is preferable.</div>
+      <div className='contentLearnFourText'>
+        <p>Scales with a wide, stable base and a built in timer are preferred. Make sure you can measure down to 1g!</p>
+      </div>
     </div>
     
   </div>
